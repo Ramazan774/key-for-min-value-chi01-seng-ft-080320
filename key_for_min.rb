44ca707abd6e6aa62d1af64f458aec30a8a386c3
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 06:31:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,11 @@ hash_3 = {}
 
 def key_for_min_value(name_hash)
 name_hash.each do |key, value|
- if value = 1
+ if value = 1||10
   puts key  
-elsif value = 17 
+elsif value = nil
+puts nil
+
 end
 end
 
