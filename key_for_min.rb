227d8df1e451150da28b5c5b690bee6262ad2d1a
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 06:23:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,9 @@
 
 hash = {:blake => 500, :ashley => 2, :adam => 1}
 hash_2 = {:blake => 10, :ashley => 50, :adam => 17}
+hash_3 = {}
 def key_for_min_value(name_hash)
-name_hash.each_key do |key, value|
+name_hash.each do |key, value|
  if value = hash{:value3} 
   puts key 
 end
