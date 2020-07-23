e129d2276650c669538642e02440f4156f440e25
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 06:16:26 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-
+hash = {:blake => 500, :ashley => 2, :adam => 1}
+hash_2 = 
 def key_for_min_value(name_hash)
 name_hash.each_key do |key, value|
  if value = hash[:value3] 
