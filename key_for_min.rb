856181d87fd773eb4700a9661a749c9c070fe194
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 04:19:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  hash = {:blake => 500, :ashley => 2, :adam => 1}
+  value = ""
   hash.each do |key, value|
     if value == hash.min()
       key_for_min_value = value
